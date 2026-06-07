5aab6f06744cd14ee616bec18aac25e1d41d57e7
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2026-06-07 10:32:24 +0900

    ASP.NET の設定ファイルのサンプルを追加した。

---
+++ asp/conf/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("server.json", false, true);
+builder.WebHost.UseKestrel(options =>
+{
+    var _ = options.Configure(builder.Configuration.GetSection("Kestrel"));
+});
+
+var app = builder.Build();
+app.MapGet("/", () => "Hello World!");
+app.Run();
