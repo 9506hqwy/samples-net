90c2159e8b3e483b0118a573b7ad9aef09014d79
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2026-06-10 19:54:57 +0900

    ZipFile のサンプルを追加した。

---
+++ archive/zip/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO.Compression;
+
+if (args.Length < 2)
+{
+    Console.Error.WriteLine("Usage: {0} [Operation]", typeof(Program).Assembly.GetName().Name);
+    Console.Error.WriteLine("");
+    Console.Error.WriteLine("[Operatioin] extract [ZIP file] [Directory]");
+    Console.Error.WriteLine("             archive [ZIP file] [file ...]");
+    Console.Error.WriteLine("             list [ZIP file]");
+    return;
+}
+
+if (string.Equals(args[0], "extract", StringComparison.OrdinalIgnoreCase) && args.Length > 2)
+{
+    var zipFileName = args[1];
+    var destination = args[2];
+
+    ZipFile.ExtractToDirectory(
+        zipFileName,
+        destination);
+}
+else if (string.Equals(args[0], "archive", StringComparison.OrdinalIgnoreCase) && args.Length > 2)
+{
+    var zipFileName = args[1];
+    var elementFiles = args.Skip(2).ToArray();
+
+    ZipFile.CreateFromDirectory(
+        elementFiles.First(),
+        zipFileName,
+        CompressionLevel.NoCompression,
+        false);
+}
+else if (string.Equals(args[0], "list", StringComparison.OrdinalIgnoreCase))
+{
+    var zipFileName = args[1];
+
+    using var archive = ZipFile.Open(zipFileName, ZipArchiveMode.Read);
+
+    foreach (var entry in archive.Entries)
+    {
+        Console.WriteLine(entry.FullName);
+    }
+}
+else
+{
+    throw new NotSupportedException(string.Join(" ", args));
+}
+
