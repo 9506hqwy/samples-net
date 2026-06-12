6578d0ec30678ac17aa899ee31977bc47b0d1491
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2026-06-12 19:17:36 +0900

    ASP.NET Razor の POST form サンプルを追加した。

---
+++ asp/page/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Page.Pages;
+
+#pragma warning disable CA1812
+internal sealed class IndexModel : PageModel
+#pragma warning restore CA1812
+{
+    [BindProperty]
+    public string? Name { get; set; }
+
+    public IActionResult OnPost()
+    {
+        Console.WriteLine($"OnPost: {Name}");
+
+        return Page();
+    }
+}
