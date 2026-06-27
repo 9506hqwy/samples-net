b7f5b3dabcddbc70d13dbcee5440dfcead83ba63
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2026-06-27 09:51:04 +0900

    systemd サービスのサンプルを追加した。

---
+++ service/systemd/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var service = Host
+    .CreateDefaultBuilder(args)
+    .UseSystemd()
+    .ConfigureServices((hostContext, services) =>
+    {
+        _ = services.AddHostedService<Service>();
+    })
+    .Build();
+
+await service.RunAsync().ConfigureAwait(false);
+
+#pragma warning disable CA1812
+internal sealed class Service : BackgroundService
+#pragma warning restore CA1812
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken).ConfigureAwait(false);
+        }
+    }
+}
